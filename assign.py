ad72e130090be6af218f68f95a24be3c8cf324ee
zminster <zach.minster@gmail.com>
2022-02-13 23:40:27 -0500

    2022 Capstone Symposium Update

--- assign.py
+++ assign.py
@@ -1,7 +1,7 @@
 import csv, re
 
 # consts
-VALID_SESSIONS = [1, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+VALID_SESSIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # helpful session data for reporting
 with open('session_caps.txt') as f:
@@ -16,16 +16,16 @@ with open('session_locations.txt') as f:
 
 # mutables
 responses = []
-students = {} # dictionary of email to [ assigned : bool, grade : string, first_name : string, last_name : string, assignment : int ]
-assignments = [[0 for i in range(len(VALID_SESSIONS))] for i in range(2)]
-
+students = {} # dictionary of email to [ filled: bool, assigned : bool, grade : string, first_name : string, last_name : string, assignment : int ]
+assignments = [0 for i in range(len(VALID_SESSIONS))]
+print(session_caps)
 
 def isUpperclassman(student):
 	return '22' in student[0] or '23' in student[0]
 
 # responses parsing
-# format: email, session 1, session 2, session 3
-# session format: "#XX DESCRIPTION"
+# format: timestamp, email, session 1, session 2, session 3
+# session format: int (corresponds to VALID_SESSIONS)
 with open('responses.csv') as f_responses:
 	r_responses = csv.reader(f_responses)
 	line_count = -1
@@ -36,7 +36,7 @@ with open('responses.csv') as f_responses:
 
 		# clean choices: only integers
 		for i in range(2, len(row)):
-			row[i] = int(re.match(r'#(\d+)', row[i]).group()[1:])
+			row[i] = int(row[i])
 
 		responses.append(row[1:])
 
@@ -50,35 +50,40 @@ with open('students.csv') as f_students:
 		if line_count == 0:	# ignore header row
 			continue
 
-		students[row[0]] = [False, False] + row[1:]	# first thing indicates assigned
+		students[row[0]] = [False, False] + row[1:]	# first pair: filled, assigned
 
 # GREEDY MATCH ROUTINE
 # apply 'em 'til you can't no more
 
 def commit_assignment(student, session):
-	assignments[0 if '11' in student[2] or '12' in student[2] else 1][session] += 1
+	assignments[session] += 1
 	student.append(session)
 
 def randomly_assign(student):
 	# find lowest filled session in appropriate list
-	assignment_category = 0 if '11' in student[2] or '12' in student[2] else 1
-	lowest = -1
-	lowest_val = 10000
-	for cap in assignments[assignment_category]:
-		if cap < lowest_val:
-			lowest = assignments[assignment_category].index(cap)
-			lowest_val = cap
-	commit_assignment(student, lowest)
+	tried = []
+	while True:
+		lowest = -1
+		lowest_val = 10000
+		for cap_index in range(len(assignments)):
+			if assignments[cap_index] < lowest_val and not cap_index in tried:
+				lowest = cap_index
+				lowest_val = assignments[cap_index]
+		if assignments[lowest] < session_caps[lowest]:
+			commit_assignment(student, lowest)
+			break
+		else:
+			tried.append(lowest)
 
 # first assign students who filled out the form
 for r_index in range(len(responses)):
 	response = responses[r_index]
 	desire_met = False
 	students[response[0]][0] = True	# they filled out the form
-	for desire in response[2:]:
+	for desire in response[1:]:
 		if not desire in VALID_SESSIONS:
 			continue
-		if assignments[0 if isUpperclassman(response) else 1][VALID_SESSIONS.index(desire)] < session_caps[VALID_SESSIONS.index(desire)]:
+		if assignments[VALID_SESSIONS.index(desire)] < session_caps[VALID_SESSIONS.index(desire)]:
 			commit_assignment(students[response[0]], VALID_SESSIONS.index(desire))
 			desire_met = True
 			break
@@ -100,3 +105,12 @@ with open('result.csv', 'w') as f:
 	writer.writerow(['Email', 'Form Completed', 'Satisfied', 'Grade', 'Advisor', 'First Name', 'Last Name', 'Session Name', 'Location'])
 	for student in students.keys():
 		writer.writerow([student] + students[student][:-1] + [session_names[students[student][-1]], session_locations[students[student][-1]]])
+
+for location in session_locations:
+	with open('rooms/' + location + '.csv', 'w') as f:
+		writer = csv.writer(f)
+		writer.writerow(['Email', 'Form Completed', 'Satisfied', 'Grade', 'Advisor', 'First Name', 'Last Name', 'Session Name', 'Location'])
+		for student in students.keys():
+			if session_locations[students[student][-1]] == location:
+				writer.writerow([student] + students[student][:-1] + [session_names[students[student][-1]], session_locations[students[student][-1]]])
+
